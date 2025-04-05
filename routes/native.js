7b4d368926d2c92030fd4bd07f184bfb2e66229f
Gicehajunior <gicehajunior76@gmail.com>
2025-04-06 02:04:50 +0300

    fix: add dashboard route, & realign routes

--- routes/native.js
+++ routes/native.js
@@ -11,19 +11,22 @@ const RouterService = require("@config/services/RouterService");
 const Routes = (BrowserWindow, ipcMain) => {    
     const Router = new RouterService(BrowserWindow, ipcMain); 
 
-    Router.post('createTable', 'AuthController@createTable', "create-table"); 
+    // DB table routes
+    Router.post('/createTable', 'AuthController@createTable'); 
 
+    // Auth routes
     Router.get('/signup', 'AuthController@registerUser'); 
     Router.get('/login', 'AuthController@loginUser'); 
-    Router.post('loginUser', 'AuthController@loginUsers', "login-response");
-    Router.post('registerUser', 'AuthController@saveUsers', "save-users");
-
+    Router.post('/loginUser', 'AuthController@loginUsers');
+    Router.post('/registerUser', 'AuthController@saveUsers');
     Router.get('/forgotpassword', 'AuthController@forgotPasswordView'); 
     Router.get('/resetpassword', 'AuthController@resetPasswordView'); 
-    Router.post('/forgot-password', 'AuthController@forgotPassword', "forgot-password-response");
-    Router.post('/reset-password', 'AuthController@resetPassword', "reset-password-response");
-
+    Router.post('/forgot-password', 'AuthController@forgotPassword');
+    Router.post('/reset-password', 'AuthController@resetPassword');
     Router.post('/logoutUser', 'AuthController@logoutUser');
+
+    // Dashboard routes
+    Router.get('/dashboard', 'DashboardController@index'); 
 } 
 
 module.exports = Routes;
