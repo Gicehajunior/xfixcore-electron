9ec91df9cb1452715e035e65ca0bb48b6e98b6ee
Gicehajunior <gicehajunior76@gmail.com>
2025-04-16 10:50:10 +0300

    fix: rm SessionService & implemented AuthService

--- config/services/AuthService.js
+++ config/services/AuthService.js
@@ -1,5 +1,46 @@
+const fs = require("fs").promises; 
 
+class AuthService {
+    constructor(storeService) {
+        this.sess;
+        this.current_directory = process.cwd(); 
+        this.storeService = storeService;
+    } 
+    
+    session(key='') {
+        try {
+            this.sess = this.storeService.get(key ? key : '');
+        } catch (error) {
+            this.sess = {};
+        }
 
+        return this.sess;
+    }
+
+    async save_session(object) {
+        const store_response = await this.storeService.set(object);
+
+        return store_response;
+    } 
+
+    async delete_session() {
+        try {
+            const session_file = this.storeService.get_data_path(); 
+            await fs.access(session_file); 
+            await fs.unlink(session_file);
+            return true;
+        } catch (err) {
+            if (err.code === 'ENOENT') { 
+                return false;
+            } else { 
+                console.error(`Error occurred while trying to remove file: ${err.message}`);
+                throw new Error(`Error occurred while trying to remove file: ${err.message}`);
+            }
+        }
+    }
+}
+
+module.exports = AuthService;
 
 
 
